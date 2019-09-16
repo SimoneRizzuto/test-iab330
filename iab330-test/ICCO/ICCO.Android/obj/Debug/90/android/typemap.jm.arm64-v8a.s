d67597da0b3f057f6deb9ffd9e19dbfa355f540e
Simorico <simorico.dev@gmail.com>
2019-09-16 18:36:29 +1000

    test commit

---
+++ iab330-test/ICCO/ICCO.Android/obj/Debug/90/android/typemap.jm.arm64-v8a.s
@@ -0,0 +1,26 @@
+	/* Data SHA1: 1ac3b13c286d3094ec0813b35242839ddaf70b17 */
+	.arch	armv8-a
+	.file	"typemap.jm.inc"
+
+	/* Mapping header */
+	.section	.data.jm_typemap,"aw",@progbits
+	.type	jm_typemap_header, @object
+	.p2align	2
+	.global	jm_typemap_header
+jm_typemap_header:
+	/* version */
+	.word	1
+	/* entry-count */
+	.word	1343
+	/* entry-length */
+	.word	262
+	/* value-offset */
+	.word	117
+	.size	jm_typemap_header, 16
+
+	/* Mapping data */
+	.type	jm_typemap, @object
+	.global	jm_typemap
+jm_typemap:
+	.size	jm_typemap, 351867
+	.include	"typemap.jm.inc"
---
+++ iab330-test/ICCO/ICCO/RSVP.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ICCO
+{
+    public partial class RSVP : ContentPage
+    {
+        public RSVP()
+        {
+            InitializeComponent();
+        }
+    }
+}
